b3fd5b62695d0faba461aac30280508aae3e8d49
svenboulanger <svenboulanger@hotmail.com>
2017-09-13 17:05:11 +0200

    Script file removes component custom fields

--- script.php
+++ script.php
@@ -1,8 +1,6 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_fields/models', 'FieldsModel');
-
 /**
  * Script file of Club component.
  *
@@ -20,6 +18,55 @@ class com_clubInstallerScript
 	
 	public function uninstall($parent)
 	{
-		echo "Uninstalled";
+		// Initialize
+		$db = JFactory::getDbo();
+		
+		// Get all fields associated with the context com_club.member
+		$query = $db->getQuery(true)
+			->select('id')
+			->from($db->qn('#__fields'))
+			->where($db->qn('context') . '=' . $db->q('com_club.member'));
+		$result = $db->setQuery($query)->loadObjectList();
+		$list = array();
+		foreach ($result as $key => $value)
+		{
+			$list[] = $value->id;
+		}
+		if (count($list) > 0)
+		{
+			$list = implode(',', $list);
+		
+			// Remove all field values associated to the context com_club.member
+			$query = $db->getQuery(true)
+				->delete($db->qn('#__fields_values'))
+				->where($db->qn('field_id') . " IN($list)");
+			if ($db->setQuery($query)->execute())
+				echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDVALUES_SUCCESS') . '</p>';
+			else
+				echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDVALUES_FAILED') . '</p>';
+			
+			// Remove all fields associated to the context com_club.member
+			$query = $db->getQuery(true)
+				->delete($db->qn('#__fields'))
+				->where($db->qn('id') . " IN($list)");
+			if ($db->setQuery($query)->execute())
+				echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDS_SUCCESS') . '</p>';
+			else
+				echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDS_FAILED') . '</p>';
+		}
+		else
+			echo '<p>' . JText::_('COM_CLUB_REMOVED_NOFIELDS') . '</p>';
+		
+		// Remove all field groups associated to the context com_club.member
+		$query = $db->getQuery(true)
+			->delete($db->qn('#__fields_groups'))
+			->where($db->qn('context') . '=' . $db->q('com_club.member'));
+		if ($db->setQuery($query)->execute())
+			echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDGROUPS_SUCCESS') . '</p>';
+		else
+			echo '<p>' . JText::_('COM_CLUB_REMOVED_FIELDGROUPS_FAILED') . '</p>';
+
+		// Finished uninstalling the component
+		echo '<p>' . JText::_('COM_CLUB_REMOVED_ASSOCIATED_CUSTOMFIELDS') . '</p>';
 	}
 }
